249f0db701c5b93b35c6cb9bfee2a4f384844ff6
tr3bol3 <fidelrojoperez5@gmail.com>
2022-09-21 11:32:14 -0400

    PruebaA

---
+++ app/config/db.py
@@ -0,0 +1,10 @@
+POSTGRESQL = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'db',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '5433'
+    }
+}
