080e9c4b3eaced86d4828b040e47e55982addcdc
tr3bol3 <fidelrojoperez5@gmail.com>
2022-09-26 16:01:02 -0400

    Arregle el modelo Estudiante y annadi uno nuevo llamado Typa y hacer que estudiante tenga un Type

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering = ['id']
 
 class Estudiante(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.CharField(max_length=150, verbose_name='Nombre(s)')
     #El unique es para que sea unico
     ci = models.CharField(max_length=11, unique=True, verbose_name='CI')
     age = models.PositiveIntegerField(default=18)
 
-def __str__(self):
-    return self.names
+    def __str__(self):
+        return self.names
 
-class Meta:
-    verbose_name = 'Estudiante'
-    verbose_name_plural = 'Estudiantes'
-    db_table = 'Estudiante'
+    class Meta:
+        verbose_name = 'Estudiante'
+        verbose_name_plural = 'Estudiantes'
+        db_table = 'estudiante'
+        ordering = ['id']
