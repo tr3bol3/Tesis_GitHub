1f66b9de0f8d415e17589a60002a9bb331e5556c
tr3bol3 <fidelrojoperez5@gmail.com>
2022-09-26 16:01:37 -0400

    Funciones de listar, modificar, insertar, eliminar

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from config.wsgi import *
+from core.erp.models import Type
 
-# Create your tests here.
+# Listar
+query = Type.objects.all()
+print(query)
+
+# Insertar
+
+#t = Type()
+#t.name = "No Becado"
+#t.save()
+
+# Edicion
+
+#try:
+#    t = Type.objects.get(pk = 2)
+#    t.name = 'Presidente'
+#    t.save()
+#except Exception as e:
+#    print(e)
+
+# Eliminacion
+
+#try:
+#    t = Type.objects.get(id=2)
+#    t.delete()
+#except Exception as e:
+#    print(e)
