618ccdd8edfc6539c3eb4b077809b19cee143ded
tr3bol3 <fidelrojoperez5@gmail.com>
2022-10-07 15:39:59 -0400

    Annadi admin.site.register(Estudiante) admin.site.register(Trabajador) admin.site.register(Guardia_Trabajador) admin.site.register(Guardia_Estudiante) admin.site.register(Guardia_Estudiante_Externo) admin.site.register(Ausencia_Estudiante) admin.site.register(Ausencia_Trabajador) admin.site.register(Corte) admin.site.register(Reporte_Mensual) para poder annadir elementos desde la 127.../admin

--- app/core/erp/admin.py
+++ app/core/erp/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from core.erp.models import *
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Estudiante)
+admin.site.register(Trabajador)
+admin.site.register(Guardia_Trabajador)
+admin.site.register(Guardia_Estudiante)
+admin.site.register(Guardia_Estudiante_Externo)
+admin.site.register(Ausencia_Estudiante)
+admin.site.register(Ausencia_Trabajador)
+admin.site.register(Corte)
+admin.site.register(Reporte_Mensual)
