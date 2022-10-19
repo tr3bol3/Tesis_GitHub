fc2e18312330bf467d6a785f3eeb074516b1048e
tr3bol3 <fidelrojoperez5@gmail.com>
2022-10-19 16:07:17 -0400

    Annadi homepage y tweaks

--- app/config/settings.py
+++ app/config/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Apps
     'core.erp',
+    'core.homepage',
+    #Libs
+    'widget_tweaks',
+
+
 ]
 
 MIDDLEWARE = [
